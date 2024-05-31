f140e4db6b869041e8e8f323249d176a1c8ffe0f
unknown <eranner@gmail.com>
2024-05-31 16:37:27 -0400

    styling for about page

--- themes/ej_tumblers_plus/page-about.php
+++ themes/ej_tumblers_plus/page-about.php
@@ -3,13 +3,14 @@
 require('includes/header.php');
 require('includes/navbar.php');
 ?>
-
+<div class="about-page-outline">
 <div class="container about-page-container" >
     <?php
 while(have_posts()) {
     the_post();
     the_content();
 };?>
+</div>
 </div>
 <?php
 require('includes/cta.php');

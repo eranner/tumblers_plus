6a280d06155c64507e85fc3081adc798f589759e
unknown <eranner@gmail.com>
2024-05-31 14:42:18 -0400

    styled results properly

--- themes/ej_tumblers_plus/search.php
+++ themes/ej_tumblers_plus/search.php
@@ -1,7 +1,6 @@
 <?php 
 require('includes/header.php');
 require('includes/navbar.php');
-
 ?>
 
 <main id="main" class="site-main">
@@ -15,26 +14,24 @@ require('includes/navbar.php');
         <div class="search-results container">
             <?php while ( have_posts() ) : the_post(); ?>
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <div class="search-card-holder">
-                <?php if ( has_post_thumbnail() ) : ?>
-                    
-                        <div class="search-thumbnail">
-                            <a href="<?php the_permalink(); ?>">
-                                <?php the_post_thumbnail( 'thumbnail' ); ?>
-                            </a>
-                        </div>
-                    <?php endif; ?>
-                    <div class="search-details">
-                        <header class="entry-header">
-                            <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                        </header>
-                        <div class="entry-summary">
-                            <?php the_excerpt(); ?>
+                    <div class="search-card-holder">
+                        <?php if ( has_post_thumbnail() ) : ?>
+                            <div class="search-thumbnail">
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php the_post_thumbnail( 'thumbnail' ); ?>
+                                </a>
+                            </div>
+                        <?php endif; ?>
+                        <div class="search-details">
+                            <header class="entry-header">
+                                <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                            </header>
+                            <div class="entry-summary">
+                                <?php the_excerpt(); ?>
+                            </div>
                         </div>
                     </div>
-
                 </article>
-                </div>
             <?php endwhile; ?>
 
             <?php the_posts_navigation(); ?>
@@ -51,6 +48,8 @@ require('includes/navbar.php');
         </section>
     <?php endif; ?>
 </main>
+
 <?php
 require('includes/cta.php');
 require('includes/footer.php');
+?>
